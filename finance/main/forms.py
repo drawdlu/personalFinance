d55f762e487f66d16ae61238ff61716088455127
Edward <edwardedeza15@gmail.com>
2024-07-07 17:29:11 +0800

    created model for category and form for adding

--- finance/main/forms.py
+++ finance/main/forms.py
@@ -1,5 +1,13 @@
 from django import forms
+from .models import Category
 
+# for creating new accounts
 class CreateNewAccount(forms.Form):
     name = forms.CharField(label="Account Name: ", max_length=200)
     balance = forms.DecimalField(max_digits=13)
+
+# for creating new categories
+class CreateNewCategory(forms.ModelForm):
+    class Meta:
+        model = Category
+        exclude = ['user']
--- finance/main/views.py
+++ finance/main/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render, redirect
-from .models import Accounts
-from .forms import CreateNewAccount
+from .models import Accounts, Category
+from .forms import CreateNewAccount, CreateNewCategory
 from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Create your views here
+
+# main page showing current months balance sheet
+@login_required(login_url="/login/")
 def home(response):
+    if response.method == "POST":
+        form = CreateNewCategory(response.POST)
+
+        # try to create new category
+        if form.is_valid():
+            n = form.cleaned_data["category_name"]
+            new = Category(category_name=n)
+            new.save()
+
+            # create category
+            try:
+                response.user.category.add(new)
+            except IntegrityError as e:
+                print(e)
+                return redirect("/")
+    else:
+        form = CreateNewCategory()
     
-    return render(response, "main/home.html", {})
+    return render(response, "main/home.html", {"form": form})
 
 # creating new accounts and showing account data
-@login_required(login_url="/accounts/")
+@login_required(login_url="/login/")
 def accounts(response):
     # user clicked ADD ACCOUNT
     if response.method == "POST":
@@ -27,7 +47,7 @@ def accounts(response):
                 response.user.accounts.add(new)
             # catch duplicates
             except IntegrityError as e:
-                print(response.user.accounts.all)
+                print(e)
                 return redirect("/accounts")
             
         return redirect("/accounts")
