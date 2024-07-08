8cd4c019ee9b557d0ccdcecfb981d1992791ba33
Edward <edwardedeza15@gmail.com>
2024-07-08 18:12:05 +0800

    completed registration validation, no email yet

--- finance/register/views.py
+++ finance/register/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django import forms
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
 
 # Create your views here.
 def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
-            form.save() # save new user into database
-        return redirect("/")
+            new_user = form.save() # save new user into database
+            messages.info(response, "Thanks for registering. You are now logged in.")
+            new_user = authenticate(username=form.cleaned_data["username"],
+                                    password=form.cleaned_data["password1"])
+            login(response, new_user)
+            return redirect("/")
     else: 
         form = RegisterForm()
     return render(response, "register.html", {"form": form})
