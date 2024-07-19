85baf2351d813cff15ec687541ea2f609aac89d8
Edward <edwardedeza15@gmail.com>
2024-07-19 23:48:41 +0800

    testing

--- finance/finance/settings.py
+++ finance/finance/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-td!&l^f&v0zc@oaocsg$r31d#jp^%pie5$)awxa605bzxunecb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
